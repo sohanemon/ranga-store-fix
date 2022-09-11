f2f3fb166e30bad464c3f80534f7b8e6cf6b9e90
sohanemon <m.sohanemon@gmail.com>
2022-09-11 22:43:47 +0600

    search bug fixed

---
+++ js/gh.js
@@ -0,0 +1,10 @@
+const person = { name: "hero", id: 101, address: "BD", postalCode: 40321 };
+console.log(Object.keys(person).length);
+const animals = ["cat", "dog", "rat"];
+console.log(animals.includes("Cat"));
+const unique = (array) => array.indexOf(21);
+
+array = [2, 3, 4, 5, 2];
+console.log(unique(array));
+const colors = { mango: "green", grapes: "black", organe: "yellow" };
+console.log(colors["grapes"]);
